6ee94ef1e2ca5a87b24979be4a1de9b2d01ec2b6
Karl Hobley <karlhobley10@gmail.com>
2015-03-06 15:47:25 +0000

    Added djcelery to INSTALLED_APPS

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -148,6 +148,7 @@ INSTALLED_APPS = (
     'taggit',
     'modelcluster',
     'django.contrib.admin',
+    'djcelery',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
