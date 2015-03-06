d3bf0f7c7c94d41235e241dc1c656c25822339fd
Karl Hobley <karlhobley10@gmail.com>
2015-03-06 15:23:00 +0000

    Add something that will crash

--- wagtailsite/urls.py
+++ wagtailsite/urls.py
@@ -2,6 +2,13 @@ from django.conf.urls import url
 
 from wagtailsite.feeds import BlogFeed
 
+def divide_by_zero(request):
+    a = 1
+    b = 0
+
+    return a / b
+
 urlpatterns = [
-    url(r'^blog/feed/$', BlogFeed(), name='blog_feed')
+    url(r'^blog/feed/$', BlogFeed(), name='blog_feed'),
+    url(r'^divide_by_zero$', divide_by_zero),
 ]
